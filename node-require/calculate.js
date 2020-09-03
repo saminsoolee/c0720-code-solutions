9742ec674c3a66b331aaeebe3de45b0547bc425f
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-03 02:19:12 +0000

    node require test

---
+++ node-require/calculate.js
@@ -0,0 +1,19 @@
+// /* eslint-disable no-console */
+// const add = require('./add');
+// const subtract = require('./subtract');
+// const multiply = require('./multiply');
+// const divide = require('./divide');
+
+// console.log(process.argv);
+
+// /* Perform conditional logic based on values passed to the program
+
+// string '2' needs to be changed to a number
+// the operations ex: add, needs to be able to take plus
+// ex: minus, needs to be able to take subtract
+// ex: times, needs to be able to take multiply
+// ex: divide, needs to be able to take over
+
+// -use parseInt to take the string and return an integer
+// -need to make the phrases equal to the variable names
+// */
