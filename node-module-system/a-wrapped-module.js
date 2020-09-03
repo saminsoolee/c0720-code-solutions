80f94c6d3b343ba6fddf10f3ba4a9e5b9d3dc3cb
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-03 00:12:51 +0000

    Completed Node Module System

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+console.log('type of exports:', typeof exports);
+console.log('value of exports:', exports);
+console.log('type of require:', typeof require);
+console.log('value of require:', require);
+console.log('type of module:', typeof module);
+console.log('value of module:', module);
+console.log('type of module:', typeof __filename);
+console.log('value of module:', __filename);
+console.log('type of module:', typeof __dirname);
+console.log('value of module:', __dirname);
