95f8dba7874f8819879ce9f2521a53d2c191da82
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-09 00:47:19 +0000

    incomplete express-sendfile

---
+++ express-sendfile/index.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const app = express();
+
+// app.use((req, res, next) => {
+//   console.dir(req.originalUrl);
+//   res.sendFile({ root: __dirname });
+// });
+
+// app.listen(3000, () => {
+//   console.log('Port 3000!');
+// });
