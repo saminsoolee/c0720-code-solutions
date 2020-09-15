54e3900d8fca30df00d9c0790f28082a92bd110e
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-15 19:06:36 +0000

    Noob Mistake Comments Left

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const element = props => {
-//   return <button>Click Me!</button>;
-// };
-// const container = document.querySelector('#root');
-
-// ReactDOM.render(element, container);
-
-// var element = function CustomButton(props) {
-//   return <button>Click Me!</button>;
-// };
-
-// var container = document.querySelector('#root');
-
-// ReactDOM.render(element, container);
-
 function CustomButton() {
   return <button>Click Me!</button>;
 }
