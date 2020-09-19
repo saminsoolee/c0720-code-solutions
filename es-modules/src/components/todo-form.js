cdd6dee70d226ce0daca3967382951f5aaf7ee24
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-19 23:52:19 +0000

    Completed ES Modules

--- es-modules/src/components/todo-form.js
+++ es-modules/src/components/todo-form.js
@@ -1,4 +1,4 @@
-const { createElement } = require('../lib');
+import { createElement } from '../lib';
 
 class TodoForm {
   constructor(onSubmit) {
@@ -44,4 +44,4 @@ class TodoForm {
   }
 }
 
-module.exports = TodoForm;
+export default TodoForm;
