d664b916c9216a13cc369f0d60464b653772ae0b
saminsoolee <samuelminsoolee71@gmail.com>
2020-09-25 04:06:16 +0000

    Finally finished Express Notes API

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,118 +1,123 @@
-// /* eslint-disable no-console */
-// const fs = require('fs');
-// const express = require('express');
-// const data = require('./data');
-// const app = express();
+/* eslint-disable no-console */
+const fs = require('fs');
+const express = require('express');
+const data = require('./data');
+const app = express();
 
-// console.log('hello');
+app.get('/api/notes', (req, res) => {
+  const notes = [];
+  for (const id in data.notes) {
+    const note = data.notes[id];
+    notes.push(note);
+  }
+  res.json(notes);
+});
 
-// app.get('/api/notes', (req, res) => {
-//   const notes = [];
-//   for (const id in data.notes) {
-//     const note = data.notes[id];
-//     notes.push(note);
-//   }
-//   res.json(notes);
-// });
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+  const note = data.notes[id];
+  if (typeof note === 'undefined') {
+    res.status(404).json({
+      error: 'id cannot be found'
+    });
+    return;
+  }
+  res.json(note);
+});
+app.use(express.json());
 
-// app.get('/api/notes/:id', (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   if (!Number.isInteger(id) || id <= 0) {
-//     res.status(400).json({
-//       error: 'id must be a positive integer'
-//     });
-//     return;
-//   }
-//   const note = data.notes[id];
-//   if (typeof note === 'undefined') {
-//     res.status(404).json({
-//       error: 'id cannot be found'
-//     });
-//     return;
-//   }
-//   res.json(note);
-// });
-// app.use(express.json());
+app.post('/api/notes', (req, res) => {
+  if (!req.body.content) {
+    res.status(400).json({
+      error: 'content is a required field'
+    });
+    return;
+  }
+  const note = {
+    id: data.nextId,
 
-// app.post('/api/notes', (req, res) => {
-//   if (!req.body.content) {
-//     res.status(400).json({
-//       error: 'content is a required field'
-//     });
-//     return;
-//   }
-//   const note = {
-//     id: data.nextId,
+    content: req.body.content
+  };
+  data.notes[data.nextId] = note;
+  data.nextId++;
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile('./data.json', json, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred'
+      });
+      return;
+    }
+    res.status(201).json(note);
+  });
+});
 
-//     content: req.body.content
-//   };
-//   data.notes[data.nextId] = note;
-//   data.nextId++;
-//   const json = JSON.stringify(data, null, 2);
-//   fs.writeFile('./data.json', json, err => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).json({
-//         error: 'An unexpected error occurred'
-//       });
-//       return;
-//     }
-//     res.status(201).json(note);
-//   });
-// });
+app.delete('/api/notes/:id', (req, res) => {
 
-// app.delete('/api/notes/:id', (req, res) => {
+  if (isNaN(req.params.id) || req.params.id < 1) {
+    res.status(400).json({ error: 'Please choose a correct number' });
+    return;
+  }
 
-//   if (isNaN(req.params.id) || req.params.id < 1) {
-//     res.status(400).json({ error: 'Please choose a correct number' });
-//     return;
-//   }
+  if (!data.notes[req.params.id]) {
+    res.status(404).json({ error: 'No matching note id' });
+    return;
+  }
+  const json = JSON.stringify(data, null, 2);
 
-//   if (!data.notes[req.params.id]) {
-//     res.status(404).json({ error: 'No matching note id' });
-//     return;
-//   }
-//   const json = JSON.stringify(data, null, 2);
+  fs.writeFile('data.json', json, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred' });
+      return;
+    }
+    res.status(204).json({});
+  });
+});
 
-//   fs.writeFile('data.json', json, err => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).json({ error: 'An unexpected error occurred' });
-//       return;
-//     }
-//     res.status(204).json({});
-//   });
-// });
+app.put('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  console.log(id);
+  if (id < 0 || isNaN(id)) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  } else if (!req.body.content) {
+    res.status(400).json({
+      error: 'Content is a required field'
+    });
+  }
+  if (!data.notes[req.params.id]) {
+    res.status(404).json({
+      error: 'cannot find note with id'
+    });
+    return;
+  }
+  const contents = req.body.content;
+  data.notes[id].content = contents;
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile('./data.json', json, err => {
+    if (err) {
 
-// app.put('api/notes/:id', (req, res) => {
-//   const id = parseInt(req.params.id, 10);
-//   console.log(id);
-//   // if (id < 0 || isNaN(id)) {
-//   if (true) {
-//     res.status(400).json({
-//       error: 'id must be a positive integer'
-//     });
-//     return;
-//   }
-//   if (!data.notes) {
-//     res.status(404).json({
-//       error: 'cannot find note with id'
-//     });
-//     return;
-//   }
-//   const json = JSON.stringify(data, null, 2);
-//   fs.writeFile('./data.json', json, err => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).json({
-//         error: 'An unexpected error occurred'
-//       });
-//       return;
-//     }
-//     res.status(200).json({});
-//   });
-// });
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred'
+      });
+      return;
+    }
+    const note = data.notes[id];
+    res.status(200).json(note);
+  });
+});
 
-// app.listen(3000, () => {
-//   console.log('Listening to port 3000!');
-// });
+app.listen(3000, () => {
+  console.log('Listening to port 3000!');
+});
