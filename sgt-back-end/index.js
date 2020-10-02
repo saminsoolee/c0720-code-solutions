0482db97eb0590163ff6fe123b11b03eb23330fa
saminsoolee <samuelminsoolee71@gmail.com>
2020-10-02 07:29:04 +0000

    Completed Delete Route

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -7,6 +7,7 @@ const app = express();
 const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
+app.use(express.json());
 
 app.get('/api/grades', (req, res, next) => {
   const sql =
@@ -41,7 +42,8 @@ app.post('/api/grades', (req, res, next) => {
   const values = [studentName, studentCourse, studentGrade];
   db.query(sql, values)
     .then(data => {
-      res.status(200).json(data.rows[0]);
+      console.log(data.rows);
+      res.status(201).json(data.rows[0]);
     })
     .catch(err => {
       console.error(err);
@@ -49,4 +51,66 @@ app.post('/api/grades', (req, res, next) => {
         error: 'An unexpected error occurred!'
       });
     });
+});
+
+app.put('/api/grades/:gradeId', (req, res, next) => {
+  const gradeId = req.params.gradeId;
+  const studentName = req.body.name;
+  const studentCourse = req.body.course;
+  const studentGrade = req.body.grade;
+  if (isNaN(gradeId) && gradeId < 1) {
+    res.status(400).json({ error: 'Pls supply valid grade or gradeId' });
+    return;
+  }
+  const sql = `update "grades",
+                set "name" = $1,
+                    "course" = $2,
+                    "grade" = $3
+                where "gradeId" = $4
+                returning *;`;
+  const values = [studentName, studentCourse, studentGrade, gradeId];
+  db.query(sql, values)
+    .then(data => {
+      if (!data.rows[0]) {
+        res.status(404).json({ error: 'GradeId not found' });
+      } else {
+        res.status(200).json(data.rows[0]);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred!'
+      });
+    });
+});
+
+app.delete('/api/grades/:gradeId', (req, res, next) => {
+  const gradeId = req.body.gradeId;
+  if (isNaN(gradeId) && gradeId < 1) {
+    res.status(400).json({ error: 'Pls supply valid grade or gradeId' });
+    return;
+  }
+  const sql = `delete from "grades"
+                where "gradeId" = $1
+                returning *`;
+  const values = [gradeId];
+  db.query(sql, values)
+    .then(data => {
+      if (!data.rows[0]) {
+        res.status(404).json({ error: 'GradeId not found' });
+      } else {
+        res.status(200).json(data.rows[0]);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred!'
+      });
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Listening to port 3000!');
 });
