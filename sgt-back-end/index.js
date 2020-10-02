1b8baeae95550196491de4b5a1070941456f641b
saminsoolee <samuelminsoolee71@gmail.com>
2020-10-02 06:01:37 +0000

    Completed Post Route

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -26,5 +26,27 @@ app.get('/api/grades', (req, res, next) => {
 });
 
 app.post('/api/grades', (req, res, next) => {
+  const studentName = req.body.name;
+  const studentCourse = req.body.course;
+  const studentGrade = req.body.grade;
+  if (!studentName || !studentCourse || !studentGrade) {
+    res.status(400).json({ error: 'Client has input incorrect value' });
+    return;
+  }
+  const sql =
+  `insert into "grades" ("name", "course", "grade")
+    values ($1, $2, $3)
+    returning *`;
 
+  const values = [studentName, studentCourse, studentGrade];
+  db.query(sql, values)
+    .then(data => {
+      res.status(200).json(data.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred!'
+      });
+    });
 });
